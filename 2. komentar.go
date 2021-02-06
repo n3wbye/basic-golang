ae41cd02cd47efdcdcc5828c13f599170b6233f8
Muhammad Aprian <n3wbye@manjaro.arkwrn.home>
2021-02-06 16:32:26 +0700

    learn hellow-world & comment in go

---
+++ 2. komentar.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// komentar inline
+	// menampilkan pesan hello world
+	fmt.Println("hello world!")
+
+	// fmt.Println("baris ini tidak dieksekusi")
+
+	/*
+		komentar multiline
+		fmt.Println("baris ini juga tidak dieksekusi")
+	*/
+}
