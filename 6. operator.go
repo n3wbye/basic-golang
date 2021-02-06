bddfcef9a805b105790e5d4a79298d88c80f7121
Muhammad Aprian <n3wbye@manjaro.arkwrn.home>
2021-02-06 17:32:42 +0700

    learn operator in go

---
+++ 6. operator.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1. Operator aritmatika
+
+	var value = (((2+6)%3)*4 - 2) / 3
+
+	// 2. Operator perbandingan
+
+	var value = (((2+6)%3)*4 - 2) / 3
+	var isEqual = (value == 2)
+
+	fmt.Printf("nilai %d (%t) \n", value, isEqual)
+
+	// 3. Operator logika
+
+	var left = false
+	var right = true
+
+	var leftAndRight = left && right
+	fmt.Printf("left && right \t(%t) \n", leftAndRight)
+
+	var leftOrRight = left || right
+	fmt.Printf("left || right \t(%t) \n", leftOrRight)
+
+	var leftReverse = !left
+	fmt.Printf("!left \t\t(%t) \n", leftReverse)
+}
